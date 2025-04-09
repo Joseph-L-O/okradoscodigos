ccd2817eec09774375ee21f9df8c1767141fb6de
joseph-l-o <josephleonardodeoliveira@gmail.com>
2025-04-08 21:49:31 -0300

    fix: fixing the logout screen

--- pages/dashboard/logout.tsx
+++ pages/dashboard/logout.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const Logout = () => {
     const router = useRouter();
-
-    localStorage.removeItem("token");
-    localStorage.removeItem("refresh_token");
-    localStorage.removeItem("expires_at");
-    router.push("/auth/signin");
-
+    useEffect(() => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("refresh_token");
+        localStorage.removeItem("expires_at");
+        router.push("/auth/signin");
+    }, [router]);
 
     return (
         <></>
